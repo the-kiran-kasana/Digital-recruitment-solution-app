a2c5d50c72b0ec5e6c46e2cc4a9345c96cd8ab96
kkasana <kkasanacoder@gmail.com>
2024-05-30 23:30:17 +0530

    create a complete home page

---
+++ public/js/scripts.js
@@ -0,0 +1,17 @@
+// document.addEventListener('scroll', function() {
+//     if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 500) {
+//         loadMoreContent();
+//     }
+// });
+
+// function loadMoreContent() {
+//     const moreContent = document.getElementById('more-content');
+//     const newFeature = document.createElement('div');
+//     newFeature.classList.add('feature-item');
+//     newFeature.innerHTML = `
+//         <img src="/images/new-feature.jpg" alt="New Feature">
+//         <h3>New Feature</h3>
+//         <p>Discover more about our latest feature that enhances your recruitment process.</p>
+//     `;
+//     moreContent.appendChild(newFeature);
+// }
